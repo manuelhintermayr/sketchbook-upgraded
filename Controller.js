b620f07f473b3b4131925cf31b9284da2fa1a8b1
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-07-12 13:35:05 +0300

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -1,2 +1,42 @@
 
-
+let Controller = {
+  
+  'pressKey' = (keyName) => {
+    
+    const keyEvent = new KeyboardEvent('keydown', {
+      'code': keyName
+    });
+
+    document.dispatchEvent(keyEvent);
+    
+  },
+  
+  'releaseKey' = (keyName) => {
+    
+    const keyEvent = new KeyboardEvent('keyup', {
+      'code': keyName
+    });
+
+    document.dispatchEvent(keyEvent);
+    
+  },
+  
+  'moveMouse' = (deltaX, deltaY) => {
+
+    let tempEvent = new MouseEvent('mousedown');
+    document.querySelector('#canvas').dispatchEvent(tempEvent);
+    
+    const mouseEvent = new MouseEvent('mousemove', {
+        'movementX': deltaX,
+        'movementY': deltaY
+    });
+  
+    document.querySelector('#canvas').dispatchEvent(mouseEvent);
+    
+    tempEvent = new MouseEvent('mouseup');
+    document.querySelector('#canvas').dispatchEvent(tempEvent);
+    
+  }
+  
+};
+
