029d66acef947f7db27193fa0e363248c615d33c
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-07-13 17:09:36 +0300

    Update gamepad-sketchbook.js

--- gamepad-sketchbook.js
+++ gamepad-sketchbook.js
@@ -1,5 +1,10 @@
 
 /* Gamepad integration for Sketchbook */
 
+controllers.on.press('a', (value) => {
 
+  if (value == 1) Controller.pressKey('keyA');
+  else Controller.releaseKey('keyA');
+
+});
 
